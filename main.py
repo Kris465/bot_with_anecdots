1e32fe04623e07e40586521a0a80e48cf7105d86
Kris465 <nataza7394@gmail.com>
2026-01-19 21:14:34 +0300

    anecdote

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 
+from random import choice
+import requests
 import asyncio
 from loguru import logger
 from dotenv import load_dotenv, find_dotenv
+from bs4 import BeautifulSoup
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
@@ -18,9 +21,23 @@ async def command_start_handler(message: Message):
     await message.answer("Привет! Я эхо-бот!")
 
 
-@dp.message()
-async def echo(message: Message):
-    await message.answer(message.text)
+# @dp.message()
+# async def echo(message: Message):
+#     await message.answer(message.text)
+
+@dp.message(Command("anekdot"))
+async def send_anekdot(message: Message):
+    response = requests.get('http://www.anekdot.ru/random/anekdot/')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        jokes = soup.find_all('div', class_='text')
+
+        random_joke = choice(jokes).text.strip()
+        anekdot = random_joke
+    else:
+        anekdot = "Не удалось получить анекдот"
+
+    await message.answer(anekdot)
 
 
 async def main():
